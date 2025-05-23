3d6894ee8fbad4f659ee28854fbe387012362034
estudaí <2011estudai2011@gmail.com>
2025-05-22 22:10:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,29 @@ const fontesParaTrocar = [
   'Kalam'
 ];
 
+const params = new URLSearchParams(window.location.search);
+const c = params.get("c");
+const title = document.getElementById("title");
+
+if (c === "exps") {
+	document.getElementById("por").href = "Português/Explicação/index.html";
+	document.getElementById("mat").href = "Matemática/Explicação";
+	document.getElementById("ing").href = "Inglês/Explicação";
+	document.getElementById("sci").href = "Ciências/Explicação";
+	document.getElementById("geo").href = "Geografia/Explicação";
+	document.getElementById("his").href = "História/Explicação";
+	document.getElementById("fil").href = "Filosofia/Explicação";
+	document.getElementById("art").href = "Artes/Explicação";
+	document.getElementById("edf").href = "Educação-Física/Explicação";
+	title.textContent = "Explicações";
+} else if (c === "rsms") {
+	title.textContent = "Resumos";
+} else if (c === "vids") {
+	title.textContent = "Vídeos";
+} else if (c === "quiz") {
+	title.textContent = "Quizzes";
+}
+
 WebFont.load({
   google: {
     families: todasFontes
@@ -39,4 +62,18 @@ WebFont.load({
   inactive: () => {
     console.error('Falha ao carregar as fontes do Google Fonts');
   }
+});
+
+const chaps = document.getElementById("chaps");
+
+window.addEventListener("scroll", () => {
+	if (chaps) {
+		if (window.scrollY >= 150) {
+			chaps.classList.add("fixed");
+			chaps.classList.remove("absolute");
+		} else {
+			chaps.classList.remove("fixed");
+			chaps.classList.add("absolute");
+		}
+	}
 });
