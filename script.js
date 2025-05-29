3bc988032296f55588cdf78102e21abd51a5accc
estudaí <2011estudai2011@gmail.com>
2025-05-28 22:19:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function irr(min,max) { /*integerRandomRange*/
 };
 
 if (c === "exps") {
-	por.href = "https://www.youtube.com";
+	por.href = "Português/Explicação";
 	title.textContent = "Explicações";
 } else if (c === "rsms") {
 	//por.href = "Português/Resumo/index.html";
