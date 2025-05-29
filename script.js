f9b08b49fbaa1d1c4f3cd22c455b2e61ba0bf30e
estudaí <2011estudai2011@gmail.com>
2025-05-28 22:01:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function irr(min,max) { /*integerRandomRange*/
 };
 
 if (c === "exps") {
-	por.href = "Português/Explicação/index.html";
+	por.href = "Português/Explicação/";
 	title.textContent = "Explicações";
 } else if (c === "rsms") {
 	//por.href = "Português/Resumo/index.html";
